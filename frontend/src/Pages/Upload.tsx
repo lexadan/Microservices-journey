9478441e61710ed15fa9c84168a51c516f49d786
Romain <romain.triozon@epitech.eu>
2022-04-18 15:59:42 -0400

    add the view information on upload

--- frontend/src/Pages/Upload.tsx
+++ frontend/src/Pages/Upload.tsx
@@ -1,15 +1,24 @@
 import React, {useState} from 'react';
 import Cookies from 'universal-cookie';
 
+interface information {
+	photoId : string,
+	userId : string,
+	_id : string,
+	__v : string 
+
+}
+
 export default function Upload(){
 	const [selectedFile, setSelectedFile] = useState();
 	const [isFilePicked, setIsFilePicked] = useState(false);
-
+	const [information, setinformation] = useState<information | undefined>();
 	const changeHandler = (event : any) => {
 		setSelectedFile(event.target.files[0]);
 	};
 
-	const handleSubmission = () => {
+	const handleSubmission = (event : any) => {
+		event.preventDefault();
 		var myHeaders = new Headers();
 		myHeaders.append("Content-Type", "image/jpeg");
 
@@ -24,8 +33,8 @@ export default function Upload(){
 		  body: file,
 		};
 		fetch("http://localhost:8081/v1/image/add", requestOptions)
-		  .then(response => response.text())
-		  .then(result => console.log(result))
+		  .then(response => response.json())
+		  .then(result => setinformation(result))
 		  .catch(error => console.log('error', error));
 	};
 
@@ -37,6 +46,13 @@ export default function Upload(){
 				<button onClick={handleSubmission}>Submit</button>
 			</div>
 			</form>
+			
+			{
+				information?.userId ? <div> Id of your album: {information.userId} </div> : <div></div>
+				}
+			{
+				information?.photoId ? <div> Id of the photo: {information.photoId} </div> : <div></div>
+			}
 		</div>
 	);
 }
