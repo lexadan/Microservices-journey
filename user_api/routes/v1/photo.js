719af50b65ed2bc2746ff8f77d7ae0a34eb880a1
Corentin Renard <corentin.renard@gmail.com>
2022-04-18 14:39:10 +0200

    [ADD]: Feed and getOnePhoto

--- user_api/routes/v1/photo.js
+++ user_api/routes/v1/photo.js
@@ -7,5 +7,9 @@ router.post('/add', service.addPhoto);
 
 router.get('/all', service.getPhotos);
 
+router.get('/:id', service.getOnePhoto);
+
+router.get('/feed/:idx', service.getFeed);
+
 
 module.exports = router;
--- user_api/services/v1/photo.js
+++ user_api/services/v1/photo.js
@@ -1,6 +1,7 @@
 const Photo = require ('../../models/photo');
 const fetch = require('node-fetch');
 const jwt = require('jsonwebtoken');
+const photo = require('../../models/photo');
 
 exports.addPhoto = async (req, res, next) => {
     const authHeader = req.headers['authorization']
@@ -53,17 +54,80 @@ exports.getPhotos = async (req, res, next) => {
         if (err) {
             return res.status(404).json('Invalid Token');
         }
-        const imgs = [];
 
-        let ids = await Photo.find({userId: user._id});
-        for (let id of ids) {
-            let response = await fetch(`http://api-service:5000/get_file?id=${id.photoId}`, requestOptions);
-            let result = await response.json()
-            if (result.status == "success") {
-                imgs.push(result.payload.files);
-                console.log(imgs);
-            }
+        let photos = await Photo.find({userId: user._id});
+        let ids = [];
+        for (let photo of photos) {
+            ids.push(photo.photoId);
         }
-        return res.status(200).json(imgs);
+        let id_string = ids.join(',');
+        console.log(id_string);
+        fetch(`http://api-service:5000/get_file?id=${id_string}`, requestOptions)
+        .then(response => response.json())
+        .then(result => {
+            if (result.status == "success") {
+                res.status(200).json(result.payload.files);
+            } else {
+                return res.status(404).json('Querry Failed');
+            }
+        })
+        .catch(error => res.status(404).json(error));
+        
     });
+}
+
+exports.getOnePhoto = async (req, res, next) => {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+
+    const imageId = req.params.id;
+
+    var requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+    };
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, async (err, user) => {
+        if (err) {
+            return res.status(404).json('Invalid Token');
+        }
+
+        try {
+            Photo.find({userId: user._id, photoId: imageId});
+        } catch (error) {
+            return res.status(404).json(error);
+        }
+        fetch(`http://api-service:5000/get_file?id=${imageId}`, requestOptions)
+        .then(response => response.json())
+        .then(result => {
+            if (result.status == "success") {
+                res.status(200).json(result.payload.files);
+            } else {
+                return res.status(404).json('Querry Failed');
+            }
+        })
+        .catch(error => res.status(404).json(error));
+        
+    });
+}
+
+exports.getFeed = async (req, res, next) => {
+
+    const idx = req.params.idx;
+
+    var requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+    };
+
+    fetch(`http://api-service:5000/get_file?last=${idx}`, requestOptions)
+    .then(response => response.json())
+    .then(result => {
+        if (result.status == "success") {
+            res.status(200).json(result.payload.files);
+        } else {
+            return res.status(404).json('Querry Failed');
+        }
+    })
+    .catch(error => res.status(404).json(error));
 }
